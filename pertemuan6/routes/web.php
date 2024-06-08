a631a278d3ef4b329dc630d5fd9342f1b2f4e5a6
insanbusted <insanbusted@gmail.com>
2024-06-09 02:14:48 +0700

    pertemuan 6 dan 7

--- pertemuan6/routes/web.php
+++ pertemuan6/routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
 });
 Route::get('/profile', [ProfileController::class, 'index']);
 Route::get('/task', [TasksController::class, 'index']);
